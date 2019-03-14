fa7154280d4541c7cc54b84ebbfc56053629001a
Andrew Bradley <cspotcode@gmail.com>
2019-03-14 12:00:30 -0400

    flow -> ts code changes

--- flow-typed/modules.d.ts
+++ flow-typed/modules.d.ts
@@ -1,14 +1,10 @@
 declare module 'is-directory' {
-  declare module.exports: {
-    (filename: string, (err: ?Error, result: boolean) => void): void,
-    sync(filename: string): boolean,
+  export = exports;
+  const exports: {
+    (
+      filename: string,
+      cb: (err: Error | undefined, result: boolean) => void
+    ): void;
+    sync(filename: string): boolean;
   };
-}
-
-declare module 'parse-json' {
-  declare module.exports: (
-    input: string,
-    reviver?: Function,
-    filename?: string
-  ) => Object;
 }
